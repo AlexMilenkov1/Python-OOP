1914eb93419878399c7de58154474f4ebdd97415
Alex Milenkov <alexmilenkov07@gmail.com>
2023-11-18 23:32:01 +0200

    exercises

---
+++ static_and_class_methods/EXE/movie_world/project/dvd.py
@@ -0,0 +1,32 @@
+class DVD:
+    def __init__(self, name: str, dvd_id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = dvd_id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    @classmethod
+    def from_date(cls, dvd_id: int, name: str, date: str, age_restriction: int):
+        data = date.split('.')[1:]
+        month, year = int(data[0]), int(data[1])
+        months = {
+            1: 'January',
+            2: 'February',
+            3:'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December'
+        }
+        return cls(name, dvd_id, year, months[month], age_restriction)
+
+    def __repr__(self):
+        return f"{self.id}: {self.name} ({self.creation_month} {self.creation_year})" \
+               f" has age restriction {self.age_restriction}. Status: {'not ' if not self.is_rented else ''}rented"
--- static_and_class_methods/EXE/photo_album.py
+++ static_and_class_methods/EXE/photo_album.py
@@ -1,7 +1,39 @@
+from math import ceil
+
+
 class PhotoAlbum:
     def __init__(self, pages: int):
         self.pages = pages
         self.photos = [[] for _ in range(pages)]
 
-    def from_photos_count(self, photos_count: int):
-        
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        return cls(ceil(photos_count / 4))
+
+    def add_photo(self, label: str):
+        for number, page in enumerate(self.photos):
+            if len(page) < 4:
+                page.append(label)
+                return f"{label} photo added successfully on page {number + 1} slot {len(page)}"
+        return "No more free slots"
+
+    def display(self):
+        separator = '-' * 11 + '\n'
+        result = separator
+        for page in self.photos:
+            result += ' '.join('[]' for _ in range(len(page))) + '\n'
+            result += separator
+        return result.strip()
+
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
