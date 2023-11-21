8e7705fc8a652603467fdf026a2101f377773af3
Alex Milenkov <alexmilenkov07@gmail.com>
2023-11-21 23:42:21 +0200

    polymorphisim and abstaction

---
+++ polymorphism_and_abstraction/shapes.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class Shape(ABC):
+    @abstractmethod
+    def calculate_area(self):
+        pass
+
+    @abstractmethod
+    def calculate_perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def calculate_area(self):
+        return pi * self.radius ** 2
+
+    def calculate_perimeter(self):
+        return 2 * pi * self.radius
+
+
+class Rectangle(Shape):
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+
+    def calculate_area(self):
+        return self.width * self.height
+
+    def calculate_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+
+circle = Circle(5)
+
+print(circle.calculate_area())
+
+print(circle.calculate_perimeter())
+
+rectangle = Rectangle(10, 20)
+
+print(rectangle.calculate_area())
+
+print(rectangle.calculate_perimeter())
